3d2845c6c5156c5e4b447f888ba731814f3f3b1a
Emp details <01fe22bca097@kletech.ac.in>
2024-03-31 23:26:21 +0530

    code on Empdetails

---
+++ Employee.java
@@ -0,0 +1,30 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
+public class Employee {
+    public String name;
+    public String dob;
+
+    public void displayName() {
+        System.out.println("Student Name: " + name);
+    }
+
+    public void displayAge(String dob) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        LocalDate birthDate = LocalDate.parse(dob, formatter);
+        LocalDate currentDate = LocalDate.now();
+        Period period = Period.between(birthDate, currentDate);
+        int age = period.getYears();
+        System.out.println("Age of student: " + age + "years");
+
+    }
+
+    public static void main(String[] args) {
+        Employee e = new Employee();
+        e.name = "Soujanya";
+        e.displayName();
+        e.displayAge("14-05-2004");
+
+    }
+}
